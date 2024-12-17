8d23e058e207d868d83293fc88e187a2b9a43c3f
s1f10210254 <s1f102102548@iniad.org>
2024-12-18 03:30:55 +0900

    page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,34 @@
+"use client";
 export default function Home() {
+  const list = ["a", "b", "c"];
+  const listClick = (item: string) => {
+    if (item === "a") {
+      console.log("a");
+    } else if (item === "b") {
+      console.log("b");
+    } else {
+      console.log("c");
+    }
+  };
+
   return (
     <div>
-      <h1>Home Page</h1>
+      <h1>Home</h1>
+      <ul style={{ display: "flex", alignItems: "center" }}>
+        {list.map((item) => (
+          <li
+            key={item}
+            style={{
+              border: "1px solid black",
+              margin: "3vw",
+              width: "20vw",
+              height: "50vh",
+            }}
+          >
+            <div onClick={() => listClick(item)}>{item}</div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
